a57aad86bbfa58492e0c70d2083a845fdd5d2fcd
NadiaMlkii <nadia.maleki.dev@gmail.com>
2026-08-16 23:13:37 +0400

    feat(webapi): add auth controller and apis

---
+++ src/backend/Trippoma.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Trippoma.Application.Users.Commands.Login;
+using Trippoma.Application.Users.Commands.Register;
+
+namespace Trippoma.WebApi.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AuthController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+}
