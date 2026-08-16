20d094815c61e9a576534c1e317e6edd99fc992f
NadiaMlkii <nadia.maleki.dev@gmail.com>
2026-08-16 19:00:13 +0400

    feat(application): add iappdbcontext interface

---
+++ src/backend/Trippoma.Application/Common/Interfaces/IAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Trippoma.Domain.Entities;
+
+namespace Trippoma.Application.Common.Interfaces;
+
+public interface IAppDbContext
+{
+    DbSet<User> Users { get; }
+    DbSet<Category> Categories { get; }
+    DbSet<Place> Places { get; }
+    DbSet<PlaceImage> PlaceImages { get; }
+    DbSet<Favorite> Favorites { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
