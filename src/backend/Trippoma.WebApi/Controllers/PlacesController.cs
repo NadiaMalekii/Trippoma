d71f211a3b7fe80a2bf1224f65f74115bce24e40
NadiaMlkii <nadia.maleki.dev@gmail.com>
2026-08-16 22:34:30 +0400

    feat(webapi): add place apis

---
+++ src/backend/Trippoma.WebApi/Controllers/PlacesController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Trippoma.Application.Places.Queries.GetAllPlaces;
+using Trippoma.Application.Places.Queries.GetPlaceById;
+
+namespace Trippoma.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlacesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public PlacesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] string? search, [FromQuery] string? city, [FromQuery] Guid? categoryId)
+    {
+        var result = await _mediator.Send(new GetAllPlacesQuery(search, city, categoryId));
+        return Ok(result);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var result = await _mediator.Send(new GetPlaceByIdQuery(id));
+        return Ok(result);
+    }
+}
