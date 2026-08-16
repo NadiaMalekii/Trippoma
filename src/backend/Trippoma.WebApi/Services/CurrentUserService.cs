6788edcde5431c058577549ee959d1d0cc64f288
NadiaMlkii <nadia.maleki.dev@gmail.com>
2026-08-16 23:14:09 +0400

    feat(webapi): add current user class

---
+++ src/backend/Trippoma.WebApi/Services/CurrentUserService.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using Trippoma.Application.Common.Interfaces;
+
+namespace Trippoma.WebApi.Services;
+
+public class CurrentUserService : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public Guid UserId
+    {
+        get
+        {
+            var idClaim = _httpContextAccessor.HttpContext?.User
+                .FindFirst(ClaimTypes.NameIdentifier)?.Value
+                ?? _httpContextAccessor.HttpContext?.User
+                .FindFirst("sub")?.Value;
+
+            if (string.IsNullOrEmpty(idClaim))
+                throw new UnauthorizedAccessException("User is not authenticated.");
+
+            return Guid.Parse(idClaim);
+        }
+    }
+}
