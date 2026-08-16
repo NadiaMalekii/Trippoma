cf32ba3c7817a038076e0fb12cb68b60d9d33758
NadiaMlkii <nadia.maleki.dev@gmail.com>
2026-08-16 19:36:58 +0400

    feat(application): add coomon classes

---
+++ src/backend/Trippoma.Application/Common/NotFoundException/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Trippoma.Application.Common.NotFoundException;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string name, object key)
+        : base($"Entity \"{name}\" ({key}) was not found.")
+    {
+    }
+}
